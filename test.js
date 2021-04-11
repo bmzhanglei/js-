7a357075ddcc2edc0c6c435f4d57963ca231dad1
bmzhanglei <115097376@qq.com>
2021-04-11 17:11:28 +0800

    js-md

--- test.js
+++ test.js
@@ -1,5 +1,134 @@
+// let create = function(obj){
+//   var F = function(){}
+//   F.prototype = obj
+//   return new F()
+// }
+
+// console.log(create({})) 
+// console.log(Object.create({}))
+
+// var obj1= new Object()
+// var obj2 = {}
+
+// console.log(Object.getPrototypeOf(obj1)===Object.prototype)
+// // console.log(a())
+// console.log(Object.prototype.__proto__)
+
+function Person(name) {
+    this.name = name
+    return undefined
+    // console.log(this)
+}
+
+Person.prototype.getName = function () {
+    return this.name
+}
+
+var objFactory = function () {
+    let obj = new Object()
+    let Constructor = [].shift.call(arguments)
+    obj.__proto__ = Constructor.prototype
+    let ret = Constructor.apply(obj, arguments)
+    console.log(Constructor)
+    // console.log(ret)
+    return obj
+}
+
+// var a = new Person("a")
+
+// console.log(a)
+
+// var a = objFactory(Person,"xxx")
+// // console.log(a.name)
+// // console.log(a.getName())
+// // console.log(Object.getPrototypeOf(a)===Person.prototype)
+
+// function t(params){
+//     "use strict"
+//     console.log(this)
+//     // console.log(params)
+// }
+
+// // var obj = {a:1}
+// t()
+// t.call(obj,"ddd")
+
+// Function.prototype.myBind = function(){
+//     let self = this
+//     let context = [...arguments].shift()
+//     let args = [...arguments].slice(1)
+//     return function(){        
+//         return self.apply(context,[...args,...arguments ]  )
+//     }
+// }
 
 
+// let obj = {name:"name"}
+// var func = function( a, b, c, d ){ 
+//     console.log ( this.name ); // 输出：sven 
+//     console.log ( [ a, b, c, d ] ) // 输出：[ 1, 2, 3, 4 ] 
+// }.myBind( obj, 1, 2 );
 
-let [a=99,[c]] = [,[3,4]]
-console.log(a,c)   //1 3  
+// func(5,6,7)
+
+// var appendDiv = function(callback){
+//     for(var i=0;i<10;i++){
+//        var h3 = document.createElement("h3")
+//        h3.innerHTML = i;
+//        document.body.appendChild(h3)
+//        if(typeof callback == "function"){
+//            callback(h3,i)
+//        }       
+//     }
+// }
+// appendDiv((node,i)=>{
+//     node.style.display = i%2==1 ? "none":"block"
+// })
+
+// var currying = function (fn) {
+//     var args = [];
+//     return function a() {
+//         if (arguments.length === 0) {
+//             return fn.apply(this, args);
+//         } else {
+//             [].push.apply(args, arguments);
+//             // console.log(arguments.callee)
+//             return a;
+//         }
+//     }
+// };
+// var cost = (function () {
+//     var money = 0;
+//     return function () {
+//         for (var i = 0, l = arguments.length; i < l; i++) {
+//             money += arguments[i];
+//         }
+//         return money;
+//     }
+// })();
+
+// var cost = currying(cost);
+
+// console.log(cost(100)(200)(200, 2)())
+
+Function.prototype.uncurrying = function () {
+    var self = this;
+    return function () {
+        let obj = [...arguments].shift()
+        let rest = [...arguments].slice(1)
+        return self.apply(obj, rest)
+    }
+}
+// Function.prototype.uncurrying = function () { 
+//     var self = this; 
+//     return function() { 
+//     var obj = Array.prototype.shift.call( arguments );
+//     return self.apply( obj, arguments ); 
+// }; 
+// };
+var push = Array.prototype.push.uncurrying();
+
+(function () {
+    push(arguments, 4);
+    console.log([...arguments]); // 输出：[1, 2, 3, 4] 
+})(1, 2, 3);
