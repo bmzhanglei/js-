6fc0904ac480708db19e7efa329da7834a895125
bmzhanglei <115097376@qq.com>
2021-06-14 22:19:43 +0800

    interview

--- test.js
+++ test.js
@@ -623,31 +623,39 @@ function trampoline(f) {
 // }
 
 
-function tco(f) {
-  var value;
-  var active = false;
-  var accumulated = [];
+// function tco(f) {
+//   var value;
+//   var active = false;
+//   var accumulated = [];
 
-  return function accumulator() {
-    accumulated.push(arguments);
-    if (!active) {
-      active = true;    
-      while (accumulated.length) {      
-        value = f.apply(this, accumulated.shift());
-      }
-      active = false;
-      return value;
-    }
-  };
+//   return function accumulator() {
+//     accumulated.push(arguments);
+//     if (!active) {
+//       active = true;    
+//       while (accumulated.length) {      
+//         value = f.apply(this, accumulated.shift());
+//       }
+//       active = false;
+//       return value;
+//     }
+//   };
+// }
+
+// var sum = tco(function(x, y) {
+//   if (y > 0) {
+//     return sum(x + 1, y - 1)
+//   }
+//   else {
+//     return x
+//   }
+// });
+
+// sum(1, 100000)
+
+var a = 1,b=2;
+function test() {
+    var b = 3
+    return new Function('c','console.log(a+b+c)') 
 }
-
-var sum = tco(function(x, y) {
-  if (y > 0) {
-    return sum(x + 1, y - 1)
-  }
-  else {
-    return x
-  }
-});
-
-sum(1, 100000)
+var t = test();
+t(4)  //window 7 
